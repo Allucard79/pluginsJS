2974b86f473d6ccbde6a073ce2b4a6e368440565
Marcin Ratajczak <maratajczak@gmail.com>
2018-12-01 16:22:09 +0100

    Add html, css and js code

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    wrapAround: true,
+    pageDots: false,
+    hash: true
+});
+
+var flkty = new Flickity('.main-carousel');
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+});
+
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray(buttons);
+
+buttonGroup.addEventListener('click', function (event) {
+
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var index = buttons.indexOf(event.target);
+    flkty.select(index);
+});
